ff5c98cac85710e1b63092a29b9176e0d2f0c2d3
crackerli <crackerli@crackerlis-MBP.lan>
2020-09-15 11:33:28 +0800

    Updates

---
+++ precache-manifest.44bc65a8b82340dccd9276624b27858c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0509d8eef4dacdacbabd4a02f27c8fed",
+    "url": "/open-shards-faucet/index.html"
+  },
+  {
+    "revision": "8e9f3c53e6760acd9e84",
+    "url": "/open-shards-faucet/static/js/2.37970953.chunk.js"
+  },
+  {
+    "revision": "2f872a310e2a1f4afaf1f9cb94b642ff",
+    "url": "/open-shards-faucet/static/js/2.37970953.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15efa371fbabe28d7523",
+    "url": "/open-shards-faucet/static/js/main.5218595c.chunk.js"
+  },
+  {
+    "revision": "3d91dd3b10654932bd12",
+    "url": "/open-shards-faucet/static/js/runtime-main.53ce1927.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/open-shards-faucet/precache-manifest.8a636ff03f1aae33a332e78abf2d338c.js"
+  "/open-shards-faucet/precache-manifest.44bc65a8b82340dccd9276624b27858c.js"
 );
 
 self.addEventListener('message', (event) => {
